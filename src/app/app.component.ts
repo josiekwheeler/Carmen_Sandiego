b96563715a5767a37aff230a0e85cb9c031e11d3
jeff <jeffrileyjr80@live.com>
2019-06-14 13:21:07 -0400

    adjusted api call for image. made variable nextLocation to use as search term

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import { PexelApiService } from './pexel-api.service'
 export class AppComponent {
   title = 'Carmen-Sandiego-FP';
   photo: any[];
+  nextLocation: string = 'Moscow';
 
   constructor(private pexelService: PexelApiService) { }
  
   ngOnInit() {
-    this.pexelService.getLocationPhoto("Paris").subscribe(response => {
+    this.pexelService.getLocationPhoto(`${this.nextLocation}`).subscribe(response => {
       this.photo = response.photos[0].src.small;
       console.log(response);
       return this.photo;
