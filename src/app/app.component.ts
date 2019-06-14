64b997c91d18705ea19284226fa7582f9187dc1e
jeff <jeffrileyjr80@live.com>
2019-06-14 14:20:41 -0400

    commented out code to pull image, as we don't need it yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,20 @@ import { PexelApiService } from './pexel-api.service'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Carmen-Sandiego-FP';
-  photo: any[];
-  nextLocation: string = 'Moscow';
-  randomIndex: number = Math.floor((Math.random() * 9));
+  // title = 'Carmen-Sandiego-FP';
+  // photo: any[];
+  // nextLocation: string = 'Moscow';
+  // randomIndex: number = Math.floor((Math.random() * 9));
 
-  constructor(private pexelService: PexelApiService) { }
+  // constructor(private pexelService: PexelApiService) { }
  
   ngOnInit() {
-    this.pexelService.getLocationPhoto(`${this.nextLocation}`).subscribe(response => {
-      console.log(this.randomIndex);
-      this.photo = response[`photos`][`${this.randomIndex}`].src.medium;
-      console.log(response);
-      return this.photo;
-    });
+    // this.pexelService.getLocationPhoto(`${this.nextLocation}`).subscribe(response => {
+    //   console.log(this.randomIndex);
+    //   this.photo = response[`photos`][`${this.randomIndex}`].src.medium;
+    //   console.log(response);
+    //   return this.photo;
+    // });
   }
 
 
