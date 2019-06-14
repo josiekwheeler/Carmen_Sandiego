f9f6ab295b3e3ee28f22004decf8ec4e4ba5ce77
jeff <jeffrileyjr80@live.com>
2019-06-14 11:19:00 -0400

    got pexel api working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PexelApiService } from './pexel-api.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Carmen-Sandiego-FP';
+  photo: any[];
+
+  constructor(private pexelService: PexelApiService) { }
+ 
+  ngOnInit() {
+    this.pexelService.getLocationPhoto("Paris").subscribe(response => {
+      this.photo = response.photos[0].src.small;
+      console.log(response);
+      return this.photo;
+    });
+  }
 }
---
+++ src/app/pexel-api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PexelApiService {
+
+  apiKey:string = "563492ad6f9170000100000128cb04362abc4b32b717e2c8e9125276";
+  location:string = "Paris"
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: `${this.apiKey}`
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getLocationPhoto(location:string) {
+    return this.http.get(`https://api.pexels.com/v1/search?query=${location}+query&per_page=1&page=1`, this.httpOptions);
+  }
+}
