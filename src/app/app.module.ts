e35e304d6b2028c3c7912223f1991a3bb49e125c
jeff <jeffrileyjr80@live.com>
2019-06-14 14:22:29 -0400

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomeComponent } from './home/home.component';
+import { Location1Component } from './location1/location1.component';
+import { Location2Component } from './location2/location2.component';
+import { Location3Component } from './location3/location3.component';
+import { Location4Component } from './location4/location4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HomeComponent,
+    Location1Component,
+    Location2Component,
+    Location3Component,
+    Location4Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/location1/location1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-location1',
+  templateUrl: './location1.component.html',
+  styleUrls: ['./location1.component.css']
+})
+export class Location1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
