299a0bfae1aaec0237204a5dbf3ac0c6d0c23c7e
Josie <josiekwheeler@gmail.com>
2019-06-16 10:11:18 -0400

    working on clue1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { Location1Component } from './location1/location1.component';
 import { Location2Component } from './location2/location2.component';
 import { Location3Component } from './location3/location3.component';
 import { Location4Component } from './location4/location4.component';
-import{ApiService} from './api.service';
-import {HttpClientModule} from '@angular/common/http';
+import { ApiService } from './api.service';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule} from '@angular/forms';
 import { RestcountryapiComponent } from './restcountryapi/restcountryapi.component';
 
@@ -16,7 +16,7 @@ import { RestcountryapiComponent } from './restcountryapi/restcountryapi.compone
 @NgModule({
   declarations: [
     AppComponent,
-  LandingPageComponent,
+    LandingPageComponent,
     HomeComponent,
     Location1Component,
     Location2Component,
