3a6facf739ffd90a060e344d752b5d1f76c4dd8c
Brittany Hardy <behardy25@gmail.com>
2019-06-17 09:45:16 -0400

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,24 @@ import { Location3Component } from './location3/location3.component';
 import { Location4Component } from './location4/location4.component';
 import { ApiService } from './api.service';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router'
 
+
+const appRoutes: Routes = [
+  { path: "landing-page", component: LandingPageComponent },
+  { path: "home", component: HomeComponent },
+  { path: "location1", component: Location1Component },
+  { path: "location2", component: Location2Component },
+  { path: "location3", component: Location3Component }
+
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-  LandingPageComponent,
+    LandingPageComponent,
     HomeComponent,
     Location1Component,
     Location2Component,
@@ -25,7 +36,8 @@ import { FormsModule} from '@angular/forms';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
 
   providers: [ApiService],
