169a61423d063e330c47fc1ce01e15907a734275
Josie <josiekwheeler@gmail.com>
2019-06-19 13:17:29 -0400

    switch all stying to global styling

--- src/app/location2/location2.component.ts
+++ src/app/location2/location2.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ClueService } from '../clue.service';
+import { PexelApiService } from '../pexel-api.service';
+import { ClockService } from '../clock.service';
 @Component({
   selector: 'location2',
   templateUrl: './location2.component.html',
@@ -7,9 +9,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Location2Component implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  clueNumber = -2;  // variable that is used for ngIfs to only show one pop-up message/clue at a time
+  time; // for timer, hold the remaining time.
+  clues;  // array to hold our clues 
+  tempClueNumber = 0;  // variable used in flyOut and goBack to save clue number so you can return to it from fly out page
+  flight = false;  //  used to toggle flight screen or not
+  nextCity = 'Detroit';  // what the next city location is
+  currentCity = 'Dubai';  // current city
+  photoURL; // variable to hold URL for random background photo
+  localClues;  // array to hold shuffled array of clues
+  randomPhoto: number = Math.floor((Math.random() * 9));  // used to get a random index number for background photo
+  randomDetroitPhoto: number = Math.floor((Math.random() * 2));  // detroit only had 3 photos, this selects on of those
+  redHerring; // a fake out location that is similar to the next city
+  wrongLocation;  // a randomw wrong option
+
+  constructor(private clueService: ClueService, private pexelService: PexelApiService, private clockService: ClockService) { }
+  // method that increases clueNumber so we can show the next clue
+  showClue() {
+    this.clueNumber = 0;
+    this.clockService.onClue();
+    this.time = this.clockService.getTime();
+    // console.log(this.clueNumber);
+  }
+  // this method temporarily saves the clue number when you bring up the flight screen, sets cluenumber to -1 so it doesn't show
+  flyOut() {
+    this.tempClueNumber = this.clueNumber;
+    this.clueNumber = -1;
+    this.flight = true;
+  }
+  // method that returns cluenumber to what it was before you click on flight screen and toggles flight back
+  goBack() {
+    this.clueNumber = this.tempClueNumber;
+    this.flight = !this.flight;
+  }
+  // increase clueNumber to display next clue
+  nextClue() {
+    this.clueNumber++;
+    this.clockService.onClue();
+    this.time = this.clockService.getTime();
   }
 
+
+  ngOnInit() {
+    // this brings in the clues from the DB and adds them to clues array on load
+    this.clueService.getClues(this.nextCity).subscribe(response => {
+      this.clues = response;
+      this.clues.push({flag: this.clues[1].countrycode});
+      // console.log(this.clues);
+    });
+    // gets a random photo for clue and adds it to clues array
+    this.pexelService.getLocationPhoto(this.nextCity).subscribe(response => {
+      this.clues.unshift({ photo: response[`photos`][`${this.randomDetroitPhoto}`].src.small });
+      // console.log(this.clues);
+    });
+    // this gets a random photo of current city to use as background image
+    this.pexelService.getLocationPhoto(this.currentCity).subscribe(response => {
+      this.photoURL = response[`photos`][`${this.randomPhoto}`].src.large;
+
+      // below is used to shuffle clues array and save it as localClues array
+      let currentIndex = this.clues.length;
+      while (0 !== currentIndex) {
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        const temporaryValue = this.clues[currentIndex];
+        this.clues[currentIndex] = this.clues[randomIndex];
+        this.clues[randomIndex] = temporaryValue;
+        this.localClues = this.clues;
+        // console.log(this.localClues);
+      }
+      // gets the redHerring option from service then a wrong city
+      this.redHerring = this.clueService.redHerring[3];
+      this.wrongLocation = this.clueService.wrongLocations[2];
+      return this.localClues;
+    });
+    this.time = this.clockService.getTime();
+  }
 }
+
