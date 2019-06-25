d34f96fcdeec84f3df90468a088d9dc5d26b71c6
jeff <jeffrileyjr80@live.com>
2019-06-24 21:36:29 -0400

    added scoring, moved clock to header

---
+++ src/app/scores/scores.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ClueService } from '../clue.service';
+
+@Component({
+  selector: 'app-scores',
+  templateUrl: './scores.component.html',
+  styleUrls: ['./scores.component.css']
+})
+export class ScoresComponent implements OnInit {
+  topTen;
+
+  constructor(private clueService: ClueService) { }
+
+  ngOnInit() {
+    this.clueService.getScores().subscribe(response => {
+      this.topTen = response;
+     });
+  }
+
+}
