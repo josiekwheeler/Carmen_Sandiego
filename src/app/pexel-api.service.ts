5a4f5a0c0927875c0b290b24283ac3a38c5e24f5
Josie <josiekwheeler@gmail.com>
2019-06-25 13:32:32 -0400

    adjusting styling

--- src/app/pexel-api.service.ts
+++ src/app/pexel-api.service.ts
@@ -11,9 +11,9 @@ export class PexelApiService {
   // second key
   // apiKey:string = "563492ad6f917000010000014a9669d68f174ec5af529706f09b0407";
   // apiKey:string = "563492ad6f91700001000001e6e48cd47bd449b78f60933192bbf8b8";
-  apiKey: string = "563492ad6f91700001000001de8fe8f0d0c347eb9f22d6366e0780fd";
+  // apiKey: string = "563492ad6f91700001000001de8fe8f0d0c347eb9f22d6366e0780fd";
 
-  // apiKey: string = "563492ad6f917000010000017412b886e2eb44d2b968e5bd22bed326";
+  apiKey: string = "563492ad6f917000010000017412b886e2eb44d2b968e5bd22bed326";
 
     photoURL;
     randomPhoto: number = Math.floor((Math.random() * 10));
